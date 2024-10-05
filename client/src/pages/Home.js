680a13a421eaa1289378b2b4512f679b6d950ee6
Nakul Bansal <nakulbansal727@gmail.com>
2024-10-05 16:13:16 -0700

    Added components

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header } from '../components/index.js'
+
+export function Home() {
+  return (
+    <div>
+        <Header />
+        <p className="text-center text-xl font-bold p-4">LETS GOOOOO</p>
+    </div>
+  );
+};
