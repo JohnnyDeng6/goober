7272d28cb0f2faaecc32e5a3691e3a4b111bcbc9
Nakul Bansal <120340476+Nakul727@users.noreply.github.com>
2024-10-06 04:05:28 -0700

    Yeah (#17)
    * AAA
    
    * :|

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 
 import './tailwind.css'
 import { Main } from './Main.js'
@@ -12,4 +12,4 @@ root.render(
       <Main />
     </BrowserRouter>
   </React.StrictMode>,
-);
+)
