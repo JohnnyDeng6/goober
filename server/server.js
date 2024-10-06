f0ed90e468749790de9c51a2c18a1e0c065576ac
Micah Baker <micahb923@gmail.com>
2024-10-06 04:54:09 -0700

    Create /api/invite route

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import logger from 'morgan';
 import dotenv from 'dotenv';
 dotenv.config(); // copies env vars in .env into process.env
 
-import { getRandomUser } from './db.js';
+import { verifyLogin, selectUser } from './db.js';
 import { setupOpenAI, sendInvites } from './invite.js';
 
 setupOpenAI();
@@ -13,21 +13,38 @@ setupOpenAI();
 const app = express();
 const PORT = 8000;
 
+async function authenticate(req, res, next) {
+  if (await verifyLogin(req.query.user, req.query.pwd)) {
+    next();
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+}
+
 app.use(logger('dev'));
 
-app.get('/', async (req, res) => {
+app.get('/api/', async (req, res) => {
   res.send("Goober API");
 })
 
-app.get('/match', async (req, res) => {
-  const firstUser = await getRandomUser();
+// GET /api/invite?user=user_id&pwd=password&amount=amnt&event=event_id
+app.get('/api/invite', authenticate, async (req, res) => {
+  try {
+    const numInvites = req.query.amount;
+    const eventId = req.query.event;
 
-  console.log('1');
-  const matchedUsers = await sendInvites(firstUser, 2, 1);
+    if (!numInvites || !eventId) {
+      res.status(400).send("Bad request");
+      return;
+    }
 
-  console.log('1');
+    const targetUser = await selectUser(req.query.user);
+    const invitedUsers = await sendInvites(targetUser, numInvites, eventId);
 
-  res.send(matchedUsers);
+    res.status(200).send(invitedUsers);
+  } catch (err) {
+    res.status(500).send(err.toString());
+  }
 });
 
 app.listen(PORT, () => {
